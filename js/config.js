c9f1a8741334b41a7e64b4691a8d92d689843608
Bhuvanesh <129824441+bhuvi2189@users.noreply.github.com>
2026-01-19 11:50:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_djeGJSZ67', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5nu26ae05pthd5sra0e5mclb9c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2r0o7r1u53c8ut0c53tlrk2mvn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
