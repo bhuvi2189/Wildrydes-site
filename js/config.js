48626a676b10e459fa014a0d0d24ec781db0d620
Bhuvanesh <129824441+bhuvi2189@users.noreply.github.com>
2026-01-19 11:20:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_djeGJSZ67', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5nu26ae05pthd5sra0e5mclb9c  client id', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5nu26ae05pthd5sra0e5mclb9c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
